3252665bcd9f6f6461ba51c8bdfbcd3ce8322d91
Sebastien Sanz de Santamaria <sebastosh@pm.me>
2019-01-01 23:29:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var name = "Joe"
-var height = "74"
+var name = "Susan"
+var height = "70"
 
 if (name === "Susan") {
     console.log("The name is correct")
@@ -7,8 +7,8 @@ if (name === "Susan") {
     console.log("Expected: Susan, Received: Joe")
 }
 
-if (height === "Susan") {
-    console.log("The name is correct")
+if (height === "70") {
+    console.log("The height is correct")
 } else {
     console.log("Expected: 70, Received: 74")
 }
