90556e93ca97a3241dcc2ccd2f80a3b10a00cd33
Sebastien Sanz de Santamaria <sebastosh@pm.me>
2019-01-01 23:18:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ var height = "74"
 if (name === "Susan") {
     console.log("The name is correct")
 } else {
-    console.log("Expected the name to be Susan")
+   console.log("Nope, not their name")
 }
+
 
 // Don't worry about this
 module.exports = { name, height
